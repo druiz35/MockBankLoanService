28377486a0064c9df79d17572f8088dbeb9c72cb
druiz35 <escobarruizd@gmail.com>
2026-02-22 10:41:21 -0500

    Initial commit

---
+++ src/test/java/com/example/mockbankloanservice/MockBankLoanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockbankloanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockBankLoanServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
