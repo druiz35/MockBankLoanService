782ccab5b8ce17794be2c87c4107ec9ce847e803
druiz35 <escobarruizd@gmail.com>
2026-02-22 23:32:36 -0500

    feat: Added config for DTO to Entity transformations

--- src/main/java/com/druiz/mockbankloanservice/config/LoanModelConfig.java
+++ src/main/java/com/druiz/mockbankloanservice/config/LoanModelConfig.java
@@ -1,4 +1,13 @@
 package com.druiz.mockbankloanservice.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class LoanModelConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
